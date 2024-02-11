953c5a6faf5447988ed1a3de2cc5b0620dd7e325
Pchiroli <pablochiroli@hotmail.com>
2024-02-11 14:35:40 -0300

    creacion del codigo para la entrega

--- index.js
+++ index.js
@@ -1,99 +1,99 @@
+let cantidadDePersonas = parseInt(prompt("Ingrese la cantidad de personas a hospedar"));
+let cantidadDeDias = parseInt(prompt("Ingrese la cantidad de días"));
+let formaDePago = prompt("Por favor, ingrese la forma de pago (efectivo/transferencia o tarjeta");
 
-let cantidadDePersonas = parseInt (prompt ("ingrese la cantidad de personas a hospedar"));
-let cantidadDeDias = parseInt(prompt("Ingrese la cantidad de dias"));
-let formaDePago = prompt("Por favor ingrese la forma de pago (efectivo/transferencia o tarjeta");
-const VALORCABANASPEQUEÑAS = 30000;
-const VALORCABANAGRANDE = 45000;
-let totalAPagar;
-function totalPersonas () {
-       if (cantidadDePersonas >= 1 && cantidadDePersonas <= 4) {
-        console.log ("Tenemos dispobles las Cabañas 1, 4 y 5");
+const cabañas = [
+    {
+        nombre: "cabaña 1",
+        cantPersonas: 2,
+        servicios: "Ropa blanca, 1 habitacion (cama matrimonial)",
+        valorNoche: 28000
+    },
+    {
+        nombre: "cabaña 2",
+        cantPersonas: 3,
+        servicios: "Ropa blanca, 2 habitaciones (cama matrimonial + cama 1 plaza)",
+        valorNoche: 32000
+    },
+    {
+        nombre: "cabaña 3",
+        cantPersonas: 4,
+        servicios: "Ropa blanca,  2 habitaciones (cama matrimonial + 2 camas 1 plaza)",
+        valorNoche: 36000
+    },
+    {
+        nombre: "cabaña 4",
+        cantPersonas: 5,
+        servicios: "Ropa blanca, 2 habitacion (cama matrimonial + 3 camas 1 plaza )",
+        valorNoche: 42000
+    },
+    {
+        nombre: "cabaña 5",
+        cantPersonas: 6,
+        servicios: "Ropa blanca, 2 habitaciones (2 cama matrimonial + 2 camas 1 plaza)",
+        valorNoche: 46000
+    },
+    {
+        nombre: "cabaña 6",
+        cantPersonas: 7,
+        servicios: "Ropa blanca,  2 habitaciones (2 cama matrimonial + 4 cama 1 plaza)",
+        valorNoche: 50000
+    },
+    { 
+        nombre: "cabaña 7",
+        cantPersonas: 8,
+        servicios: "Ropa blanca,  2 habitaciones (2 cama matrimonial + 4 cama 1 plaza)",
+        valorNoche: 55000
+    }
+]
+
+function buscarCabañaDisponible(cantidadDePersonas) {
+    return cabañas.find(cabaña => cabaña.capacidad >= cantidadDePersonas);
 }
-    else if (cantidadDePersonas >= 5 && cantidadDePersonas <= 8) {
-        console.log ("Tenemos dispobles las Cabañas 2, 3 y 6");
-    }
-    else if (cantidadDePersonas <= 0 ) {
-        console.log ("Ingrese un numero valido de personas");
-    }
-    else {
-        console.log ("Tendra que alquilar 2 cabañas");
-    }
+
+function calcularCostoEstadia(cantidadDeDias, cabaña) {
+    return cantidadDeDias * cabaña.valorNoche;
 }
-totalPersonas (); 
 
-
-
-
-function costoEstadia (){
-    if (cantidadDePersonas >= 1 && cantidadDePersonas <= 4) {
-        totalAPagar = (cantidadDeDias * VALORCABANASPEQUEÑAS);
-    } else if (cantidadDePersonas >= 5 && cantidadDePersonas <= 8) {
-        totalAPagar = (cantidadDeDias * VALORCABANAGRANDE);
-    }else {
-        totalAPagar = (cantidadDeDias * (VALORCABANASPEQUEÑAS * 2 * 0.9));
-    }
-
+function aplicarRecargoPorTarjeta(totalAPagar) {
+    const recargoPorTarjeta = 1.05;
+    return totalAPagar * recargoPorTarjeta;
 }
-costoEstadia ();
 
+function obtenerMesDeReserva(fechadereserva) {
+    const meses = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
+    return meses.includes(fechadereserva) ? fechadereserva : "mes no válido";
+}
 
-if (formaDePago == "tarjeta" ){
-    let confirTarjeta = confirm("El pago con tarjeta tiene 5% de recargo, desea continuar");
-    if (confirTarjeta) {
-        console.log ("Pago confirmado con su tarjeta");
-        totalAPagar = totalAPagar * 1.05;
+if (cantidadDePersonas > 0 && cantidadDeDias > 0) {
+    const cabañaDisponible = buscarCabañaDisponible(cantidadDePersonas);
+
+    if (cabañaDisponible) {
+        let totalAPagar = calcularCostoEstadia(cantidadDeDias, cabañaDisponible);
+
+        if (formaDePago === "tarjeta") {
+            const confirTarjeta = confirm("El pago con tarjeta tiene 5% de recargo, ¿desea continuar?");
+            if (confirTarjeta) {
+                totalAPagar = aplicarRecargoPorTarjeta(totalAPagar);
+                console.log("Pago confirmado con tarjeta");
+            } else {
+                console.log("El pago no se realizó");
+            }
+        } else if (formaDePago === "efectivo") {
+            console.log("Usted abonará al ingresar");
+        } else if (formaDePago === "transferencia") {
+            console.log("Lo contactaremos para enviarle el CBU");
+        } else {
+            console.log("Forma de pago no válida");
+        }
+
+        console.log(`El total a pagar es $${totalAPagar}`);
     } else {
-        console.log("Ingrese otra forma de pago");
+        console.log("No hay cabañas disponibles para la cantidad de personas ingresada");
     }
-} else if (formaDePago == "efectivo") {
-    console.log ("Usted abonará al ingresar");
-} else if (formaDePago == "transferencia") {
-    console.log ("Lo contactaremos para enviarle el CBU");
 } else {
-    console.log ("Ingrese otra forma de pago");
+    console.log("Ingrese una cantidad válida de personas y días");
 }
 
-console.log("El total a pagar es $" + totalAPagar);
-
-const FECHADERESERVA = prompt("ingrese el mes de la reserva");
-switch (FECHADERESERVA) {
-    case "enero": 
-        console.log ("El mes seleccionado es Enero")
-        break;
-    case "febrero" :
-        console.log("El mes seleccionado es Febrero")
-        break
-    case "marzo" :
-        console.log("El mes seleccionado es Marzo")
-        break
-    case "abril" :
-        console.log("El mes seleccionado es Abril")
-        break
-    case "mayo" :
-        console.log("El mes seleccionado es Mayo")
-        break
-    case "junio" :
-        console.log("El mes seleccionado es Junio")
-        break
-    case "julio" :
-        console.log("El mes seleccionado es Julio")
-        break
-    case "agosto" :
-        console.log("El mes seleccionado es Agosto")
-        break
-    case "septiembre" :
-        console.log("El mes seleccionado es Septiembre")
-        break
-    case "octubre" :
-        console.log("El mes seleccionado es Octubre")
-        break
-    case "noviembre" :
-        console.log("El mes seleccionado es Noviembre")
-        break
-    case "diciembre" :
-        console.log("El mes seleccionado es Diciembre")
-        break
-    default : 
-        console.log("ingrese un mes valido")
-        };
-
+let fechadereserva = prompt("Ingrese el mes de la reserva");
+console.log(`El mes seleccionado es ${obtenerMesDeReserva(fechadereserva)}`);
