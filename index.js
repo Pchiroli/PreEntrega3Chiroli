1a4f638619dd331b64b6d09716c031e57fbec894
Pchiroli <pablochiroli@hotmail.com>
2024-02-12 11:23:24 -0300

    ajuste en const

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let cantidadDePersonas = parseInt(prompt("Ingrese la cantidad de personas a hosp
 let cantidadDeDias = parseInt(prompt("Ingrese la cantidad de días"));
 let formaDePago = prompt("Por favor, ingrese la forma de pago (efectivo/transferencia o tarjeta");
 
-const cabañas = [
+const CABANIAS = [
     {
         nombre: "cabaña 1",
         capacidad: 2,
@@ -47,33 +47,33 @@ const cabañas = [
     }
 ]
 
-function buscarCabañaDisponible(cantidadDePersonas) {
-    return cabañas.find(cabaña => cabaña.capacidad >= cantidadDePersonas);
+function buscarCabaniaDisponible(cantidadDePersonas) {
+    return CABANIAS.find(cabania => cabania.capacidad >= cantidadDePersonas);
 }
 
-function calcularCostoEstadia(cantidadDeDias, cabaña) {
-    return cantidadDeDias * cabaña.valorNoche;
+function calcularCostoEstadia(cantidadDeDias, cabania) {
+    return cantidadDeDias * cabania.valorNoche;
 }
 
 function aplicarRecargoPorTarjeta(totalAPagar) {
-    const recargoPorTarjeta = 1.05;
-    return totalAPagar * recargoPorTarjeta;
+    const RECARGOPORTARJETA = 1.05;
+    return totalAPagar * RECARGOPORTARJETA;
 }
 
 function obtenerMesDeReserva(fechadereserva) {
-    const meses = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
-    return meses.includes(fechadereserva) ? fechadereserva : "mes no válido";
+    const MESES = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
+    return MESES.includes(fechadereserva) ? fechadereserva : "mes no válido";
 }
 
 if (cantidadDePersonas > 0 && cantidadDeDias > 0) {
-    const cabañaDisponible = buscarCabañaDisponible(cantidadDePersonas);
+    const CABANIADISPONIBLE = buscarCabaniaDisponible(cantidadDePersonas);
 
-    if (cabañaDisponible) {
-        let totalAPagar = calcularCostoEstadia(cantidadDeDias, cabañaDisponible);
+    if (CABANIADISPONIBLE) {
+        let totalAPagar = calcularCostoEstadia(cantidadDeDias, CABANIADISPONIBLE);
 
         if (formaDePago === "tarjeta") {
-            const confirTarjeta = confirm("El pago con tarjeta tiene 5% de recargo, ¿desea continuar?");
-            if (confirTarjeta) {
+            const CONFIRTARJETA = confirm("El pago con tarjeta tiene 5% de recargo, ¿desea continuar?");
+            if (CONFIRTARJETA) {
                 totalAPagar = aplicarRecargoPorTarjeta(totalAPagar);
                 console.log("Pago confirmado con tarjeta");
             } else {
